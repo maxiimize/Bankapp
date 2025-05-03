12d84ec5f6ca80a0894094db0a052cfacc6af039
maxiimize <max.b@hotmail.se>
2025-05-03 20:24:13 +0200

    Added Services again after fail in old test branch

---
+++ Services/Services/CustomerService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly BankAppDataContext _context;
+
+        public CustomerService(BankAppDataContext context)
+        {
+            _context = context;
+        }
+
+        public CustomerOverviewVM GetCustomerOverview(int customerId)
+        {
+            var customer = _context.Customers
+                .Where(c => c.CustomerId == customerId)
+                .Select(c => new CustomerOverviewVM
+                {
+                    CustomerId = c.CustomerId,
+                    Givenname = c.Givenname,
+                    Surname = c.Surname,
+                    Streetaddress = c.Streetaddress,
+                    City = c.City,
+                    TotalBalance = c.Dispositions
+                        .Where(d => d.Account != null)
+                        .Select(d => d.Account.Balance)
+                        .Sum(),
+                    Accounts = c.Dispositions
+                        .Where(d => d.Account != null)
+                        .Select(d => new AccountSummaryVM
+                        {
+                            AccountId = d.Account.AccountId,
+                            Frequency = d.Account.Frequency,
+                            Balance = d.Account.Balance
+                        }).ToList()
+                }).FirstOrDefault();
+
+            return customer;
+        }
+
+        public PagedResult<CustomerOverviewVM> SearchCustomers(string name, string city, int pageNumber, int pageSize)
+        {
+            var query = _context.Customers.AsQueryable();
+
+            if (!string.IsNullOrEmpty(name))
+                query = query.Where(c => c.Givenname.Contains(name) || c.Surname.Contains(name));
+
+            if (!string.IsNullOrEmpty(city))
+                query = query.Where(c => c.City.Contains(city));
+
+            var totalCount = query.Count();
+            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+            var customers = query
+                .OrderBy(c => c.CustomerId)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Select(c => new CustomerOverviewVM
+                {
+                    CustomerId = c.CustomerId,
+                    Givenname = c.Givenname,
+                    Surname = c.Surname,
+                    Streetaddress = c.Streetaddress,
+                    City = c.City,
+                    TotalBalance = c.Dispositions
+                        .Where(d => d.Account != null)
+                        .Select(d => d.Account.Balance)
+                        .Sum()
+                }).ToList();
+
+            return new PagedResult<CustomerOverviewVM>
+            {
+                Results = customers,
+                CurrentPage = pageNumber,
+                TotalPages = totalPages,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+    }
+
+}
---
+++ Services/Services/StatisticsService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly BankAppDataContext _context;
+
+        public StatisticsService(BankAppDataContext context)
+        {
+            _context = context;
+        }
+
+        public StatisticsVM GetStatistics()
+        {
+            var totalCustomers = _context.Customers.Count();
+            var totalAccounts = _context.Accounts.Count();
+
+            var balancePerCountry = _context.Customers
+                .GroupBy(c => c.Country)
+                .Select(g => new
+                {
+                    Country = g.Key,
+                    TotalBalance = g.SelectMany(c => c.Dispositions)
+                                    .Where(d => d.Account != null)
+                                    .Select(d => d.Account.Balance)
+                                    .Sum()
+                })
+                .ToDictionary(x => x.Country, x => x.TotalBalance);
+
+            return new StatisticsVM
+            {
+                TotalCustomers = totalCustomers,
+                TotalAccounts = totalAccounts,
+                TotalBalancePerCountry = balancePerCountry
+            };
+        }
+    }
+
+}
