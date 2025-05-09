471d9d191125685b1ece13559e17cb85b43a69f8
maxiimize <max.b@hotmail.se>
2025-05-09 22:22:07 +0200

    Update DataInitializer and enhance README documentation
    Comment out database migration in DataInitializer.cs to prevent automatic migrations during seeding.
    
    Significantly update README.md to provide a detailed overview of the application, including features, roles, seeded users, and technologies used.

--- DataAccessLayer/Models/DataInitializer.cs
+++ DataAccessLayer/Models/DataInitializer.cs
@@ -21,7 +21,7 @@ namespace DataAccessLayer.Models
         }
         public void SeedData()
         {
-            _dbContext.Database.Migrate();
+            //_dbContext.Database.Migrate();
             SeedRoles();
             SeedUsers();
         }
